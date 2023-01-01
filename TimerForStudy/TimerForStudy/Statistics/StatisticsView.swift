0a16c6fce1237bd4e94169462f9ff706076305da
Donguk Kim <ehddnr73@naver.com>
2023-01-02 03:57:50 +0900

    SwiftUI View, StatisticsView를 StatisticsViewController에 Embed
    - StatisticsView에서 다양한 통계 assembling

---
+++ TimerForStudy/TimerForStudy/Statistics/StatisticsView.swift
@@ -0,0 +1,20 @@
+//
+//  StatisticsView.swift
+//  TimerForStudy
+//
+//  Created by 김동욱 on 2023/01/02.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+struct StatisticsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticsView()
+    }
+}
