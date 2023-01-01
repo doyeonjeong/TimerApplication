e2dd24e8f40f443ff84013e59529794149e7059f
Donguk Kim <ehddnr73@naver.com>
2023-01-02 08:47:23 +0900

    StatisticsView에 EnvironmentObject 주입하기 위해 임시로 mock object를 생성하여 반환하는 함수 추가

--- TimerForStudy/TimerForStudy/Statistics/Views/StatisticsViewController.swift
+++ TimerForStudy/TimerForStudy/Statistics/Views/StatisticsViewController.swift
@@ -18,6 +18,7 @@ class StatisticsViewController: UIViewController {
     // Embed SwiftUI View
     private func configureAndEmbedView() {
         let statisticsView = StatisticsView()
+            .environmentObject(Self.createMockObject())
         let hostingController = UIHostingController(rootView: statisticsView)
         
         self.addChild(hostingController)
@@ -31,5 +32,19 @@ class StatisticsViewController: UIViewController {
             hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
             hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+    
+    // SwiftUI View Model에 주입할 통계 데이터 가져오는 함수
+    private func fetchStatData() { }
+    
+    // For test
+    // mock object 생성해서 주입
+    static func createMockObject() -> Statistics {
+        let programmingData = Subject(name: "프로그래밍", total: 2000)
+        let algorithmData = Subject(name: "알고리즘", total: 1100)
+        let operatingSystemData = Subject(name: "운영체제", total: 900)
+        let model = Statistics(name: "Yeolmok", total: 4000, subjects: [programmingData, algorithmData, operatingSystemData])
+        
+        return model
     }
 }
