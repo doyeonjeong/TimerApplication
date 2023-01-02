0320b4bb5a759b9db3b7f3d842339c5b064798e1
Debby <debby_@kakao.com>
2023-01-02 18:34:35 +0900

    🚚 Rename Controller & Folder : Profile -> User

---
+++ TimerForStudy/TimerForStudy/User/UserMainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  TimerForStudy
+//
+//  Created by 김동욱 on 2022/12/11.
+//
+
+import UIKit
+
+class UserMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.view.backgroundColor = .systemBackground
+    }
+}
