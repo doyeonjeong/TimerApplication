b565c5d997626245f66fbb5f1ad2e37e4130930a
Donguk Kim <ehddnr73@naver.com>
2023-01-07 08:38:14 +0900

    카테고리별 공부 시간 %를 나타내는 차트를 PercentageChartsView로 분리

---
+++ TimerForStudy/TimerForStudy/Stat/Views/PercentageChartsView.swift
@@ -0,0 +1,45 @@
+//
+//  PercentageChartsView.swift
+//  TimerForStudy
+//
+//  Created by 김동욱 on 2023/01/07.
+//
+
+import SwiftUI
+import Charts
+
+struct PercentageChartsView: View {
+    let dailyData: Daily
+    
+    var body: some View {
+        Chart(dailyData.subjects) { subject in
+            BarMark(
+                x: .value(TextConstants.percentageLabel, subject.time/dailyData.total * 100),
+                y: .value(TextConstants.singleLabel, TextConstants.singleLabelText),
+                width: .fixed(50),
+                stacking: .standard
+            )
+            .foregroundStyle(by: .value(TextConstants.percentageLabel, subject.name))
+            .annotation(position: .overlay, alignment: .center) {
+                Text("\(Int(subject.time/dailyData.total * 100))%")
+            }
+        }
+        .padding(LayoutConstants.padding)
+    }
+}
+
+private enum TextConstants {
+    static let percentageLabel = "Percentage"
+    static let singleLabel = "Single"
+    static let singleLabelText = "카테고리별 비율, 소수점 이하는 버림"
+}
+
+private enum LayoutConstants {
+    static let padding: CGFloat = 8
+}
+
+struct PercentageChartsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PercentageChartsView(dailyData: statRowData.monthlyData[0].dailyData[0])
+    }
+}
