b8c2d4bf0afa831665874f5e665c009f0c7f923d
Donguk Kim <ehddnr73@naver.com>
2023-01-10 01:50:35 +0900

    Friend에 isAccessEnabled 프로퍼티 추가에 따른 수정
    - 정보 접근을 허용하지 않을 때, 보여줄 이미지 아이콘 추가 및 분기 수정

--- TimerForStudy/TimerForStudy/Stat/Views/FriendRow.swift
+++ TimerForStudy/TimerForStudy/Stat/Views/FriendRow.swift
@@ -14,7 +14,9 @@ struct FriendRow: View {
         HStack {
             friend.image
                 .resizable()
-                .frame(width: 50, height: 50)
+                .frame(width: LayoutConstants.profileImageLength,
+                       height: LayoutConstants.profileImageLength)
+            
             Text(friend.name)
             
             Spacer()
@@ -22,17 +24,32 @@ struct FriendRow: View {
             Button(action: {
                 friend.isPeeping.toggle()
             }, label: {
-                if friend.isPeeping {
-                    Image(systemName: "eyes")
-                        .foregroundColor(.black)
+                if friend.isAccessEnabled && friend.isPeeping {
+                    Image(systemName: TextConstants.eyesIconName)
+                        .foregroundColor(.blue)
+                } else if friend.isAccessEnabled && !friend.isPeeping {
+                    Image(systemName: TextConstants.eyesIconName)
+                        .foregroundColor(.gray)
+                } else {
+                    Image(systemName: TextConstants.xCircleiconName)
+                        .foregroundColor(.red)
                 }
             })
         }
     }
 }
 
+private enum LayoutConstants {
+    static let profileImageLength: CGFloat = 50
+}
+
+private enum TextConstants {
+    static let eyesIconName = "eyes"
+    static let xCircleiconName = "x.circle"
+}
+
 struct FriendRow_Previews: PreviewProvider {
     static var previews: some View {
-        FriendRow(friend: .constant(.init(name: "김동욱", image: Image("User1"), isPeeping: true)))
+        FriendRow(friend: .constant(.init(name: "김동욱", image: Image("User1"), isAccessEnabled: true, isPeeping: true)))
     }
 }
