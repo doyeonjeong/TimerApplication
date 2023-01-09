dff908eaa7c96808baf9614973d3353eef47af40
Donguk Kim <ehddnr73@naver.com>
2023-01-09 23:28:12 +0900

    탭을 통해 종합 차트에서 친구의 정보를 추가하거나 삭제하기 위한 FriendRow 추가

--- TimerForStudy/TimerForStudy/Stat/Views/FriendRow.swift
+++ TimerForStudy/TimerForStudy/Stat/Views/FriendRow.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct FriendRow: View {
-    @ObservedObject var friend: Friend
+    @Binding var friend: Friend
     
     var body: some View {
         HStack {
@@ -16,17 +16,15 @@ struct FriendRow: View {
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(friend.name)
+            
             Spacer()
             
             Button(action: {
-                friend.isFavorite.toggle()
+                friend.isPeeping.toggle()
             }, label: {
-                if friend.isFavorite {
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.yellow)
-                } else {
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.gray)
+                if friend.isPeeping {
+                    Image(systemName: "eyes")
+                        .foregroundColor(.black)
                 }
             })
         }
@@ -35,6 +33,6 @@ struct FriendRow: View {
 
 struct FriendRow_Previews: PreviewProvider {
     static var previews: some View {
-        FriendRow(friend: .init(name: "김동욱", image: Image(systemName: "person.crop.circle"), isFavorite: true))
+        FriendRow(friend: .constant(.init(name: "김동욱", image: Image("User1"), isPeeping: true)))
     }
 }
